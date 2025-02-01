ceb7e3089a33c7ad454c4423d39d55fe6554c4e8
Rohan Sohini <138546171+rohansohini@users.noreply.github.com>
2025-01-31 19:11:06 -0600

    changed time

--- slurm_run.sh
+++ slurm_run.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --account=<account> ## Required: your allocation/account name, i.e. eXXXX, pXXXX or bXXXX
 #SBATCH --partition=short ## Required: (buyin, short, normal, long, gengpu, genhimem, etc)
-#SBATCH --time=00:04:00 ## Required: How long will the job need to run
+#SBATCH --time=04:00:00 ## Required: How long will the job need to run
 #SBATCH --nodes=1 ## how many computers/nodes do you need
 #SBATCH --ntasks-per-node=28 ## how many cpus or processors (should match ncores in params.txt)
 #SBATCH --mem=50G ## how much memory per node
